6e772bc9fa65e24b64b1477beb2631c300840c03
Balaji P <balaji.pn@prodapt.com>
2022-11-04 11:32:27 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BirthdayComponent } from './birthday/birthday.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'birthday',component:BirthdayComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import * as material from '@angular/material';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class MaterialModule { }
