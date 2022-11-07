98d22dada6a05d9d7b78da777698a05e63ad2007
Balajiwd21 <balajiwd21@gmail.com>
2022-11-07 15:23:29 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BirthdayComponent } from './birthday/birthday.component';
+import { ConcertComponent } from './concert/concert.component';
 import { HomeComponent } from './home/home.component';
+import { WeddingComponent } from './wedding/wedding.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
-  {path:'birthday',component:BirthdayComponent}
+  {path:'birthday',component:BirthdayComponent},
+  {path:'wedding',component:WeddingComponent},
+  {path:'concert',component:ConcertComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BirthdayComponent } from './birthday/birthday.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { WeddingComponent } from './wedding/wedding.component';
+import { ConcertComponent } from './concert/concert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BirthdayComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    WeddingComponent,
+    ConcertComponent
   ],
   imports: [
     BrowserModule,
